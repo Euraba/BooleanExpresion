6f1f186d18f6381fa120a308792a96e2c4064ac1
Euraba <43251950+Euraba@users.noreply.github.com>
2021-03-03 16:44:42 +0200

    Create polygon.h

---
+++ polygon.h
@@ -0,0 +1,71 @@
+//
+// Created by she on 03.03.2021.
+//
+
+#ifndef TURNURI4_POLYGON_H
+#define TURNURI4_POLYGON_H
+
+
+#include <vector>
+#include <iostream>
+
+struct Point {
+    int x, y ;
+    Point() {} ;
+    Point(int _x, int _y) {
+        this ->x = _x ;
+        this ->y = _y ;
+    }
+
+    bool operator == (const Point & aux) {
+        return (this ->x == aux.x && this ->y == aux.y) ;
+    }
+
+    bool operator != (const Point &aux) {
+        return !(*this == aux) ;
+    }
+};
+
+class Polygon {
+private :
+    std::vector<Point> points ;
+    int orientation ;
+public :
+    Polygon(const std::vector <Point> &points) : points(points) {}
+
+    int area() {
+        int ret(0) ;
+        for (int i = 0 ; i < points.size() ; ++ i) {
+            ret += (points[i].x * points[i + 1].y) - (points[i].y * points[i + 1].x) ;
+        }
+        if (ret < 0) {
+            orientation = 0 ;
+            ret *= -1 ;
+        } else orientation = 1 ;
+        return ret / 2 ;
+    }
+
+    void add(Point aux) {
+        points.push_back(aux) ;
+    }
+
+    void complete() {
+        if (points.back() != points.front()) {
+            points.push_back(points[0]) ;
+        }
+    }
+
+    int getOrientation() {
+        return orientation ;
+    }
+
+    void debug() {
+        for (auto it : points) {
+            std::cerr << it.x << ' ' << it.y << '\n' ;
+        }
+    }
+
+};
+
+
+#endif //TURNURI4_POLYGON_H
